98d0e53bbf2d9ab4b6fca9121934a4078ad9d94e
P Deepanvitha Sri Sai <anvithapadmanabhuni@gmail.com>
2025-09-10 19:18:10 +0530

    Add files via upload
    Enter a number
    3423523
    7

---
+++ NumberOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class NumberOfDigits{
+    int noOfDigits(int n){
+        int count=0;
+        while(n!=0){
+            int r=n%10;
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        NumberOfDigits nd=new NumberOfDigits();
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        System.out.println(nd.noOfDigits(n));
+    }
+}
