dcb7f048cf9a53a5abbd6abf55820436ffbbbaa5
P Deepanvitha Sri Sai <anvithapadmanabhuni@gmail.com>
2025-09-12 08:05:19 +0530

    Add files via upload
    Enter a number
    16
    [1, 2, 4, 8, 16]

---
+++ DivisorsOfNumber2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.*;
+public class DivisorsOfNumber2 {
+    void divisorsOfN2(int n){
+        ArrayList<Integer> div=new ArrayList<>();
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                div.add(i);
+                if(i!=n/i)
+                div.add(n/i);
+            }
+        }
+        Collections.sort(div);
+        System.out.println(div);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        DivisorsOfNumber2 dn=new DivisorsOfNumber2();
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        dn.divisorsOfN2(n);
+    }
+}
